4c91554e2895072cf56a99eaf302390b46cfb8d8
AdamAudio7X <peterklimqa@gmail.com>
2023-08-15 21:49:02 -0700

    First Commit

---
+++ Sun/src/test/java/TestPK/TestPK_01.java
@@ -0,0 +1,15 @@
+package TestPK;
+
+public class TestPK_01 {
+
+	public static void PrintHello()
+	{
+		System.out.println("Hello");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		PrintHello();
+	}
+
+}
